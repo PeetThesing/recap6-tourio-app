c64b4da62cffcd5f10706cf2cfe34fafca51f3a3
Peet Thesing <p.thesing@posteo.de>
2023-09-26 13:18:22 +0200

    add create functionality

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -19,11 +19,3 @@ export default async function handler(request, response) {
     response.status(200).json(place);
   }
 }
-
-// const place = places.find((place) => place.id === id);
-
-// if (!place) {
-//   return response.status(404).json({ status: "Not found" });
-// }
-
-// response.status(200).json(place);
